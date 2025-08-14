3693b2f52f96bdb6cd92a6deedfc9a543389e904
Mikhail Reznichenko <justmarfix@vk.com>
2025-08-15 01:41:19 +0300

    hotfix(exam): fix exam type

--- backend/src/domain/exam/model.rs
+++ backend/src/domain/exam/model.rs
@@ -17,6 +17,7 @@ pub struct Exam {
 
 #[derive(Serialize, Deserialize, sqlx::Type, ToSchema)]
 #[sqlx(type_name = "EXAM_TYPE")]
+#[serde(rename_all = "UPPERCASE")]
 pub enum ExamType {
     Instant,
     Delayed,
