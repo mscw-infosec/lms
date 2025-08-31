b4b2b21fcaacb9e30bbebcf6bf9fd904f03139c8
JustMarfix <rezak3031@ya.ru>
2025-08-31 22:35:56 +0300

    update landing

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -16,7 +16,19 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<title>infosec.moscow</title>
+                <!-- Yandex.Metrika counter -->
+                <script type="text/javascript">
+                    (function(m,e,t,r,i,k,a){
+                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103961320', 'ym');
+
+                    ym(103961320, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+                </script>
+                <noscript><div><img src="https://mc.yandex.ru/watch/103961320" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                <!-- /Yandex.Metrika counter -->
 				<script defer src="https://umami.justmarfix.ru/script.js" data-website-id="7551edd5-225b-4490-a0b0-6cfb1a6a13d8"></script>
 				<style>{`
 html {
@@ -26,7 +38,7 @@ html {
 }
         `}</style>
 				<meta property="og:title" content="infosec.moscow" />
-				<meta property="og:description" content="Сборная Москвы на ВсОШ по ИБ" />
+				<meta property="og:description" content="Занятия по информационной безопасности для школьников" />
 				<meta property="og:type" content="website" />
 				<meta property="og:url" content="https://infosec.moscow/" />
 				<meta property="og:image" content="https://infosec.moscow/favicon.ico" />
